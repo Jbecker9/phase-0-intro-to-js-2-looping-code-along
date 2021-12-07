ff25871513688aaca94ee0122d74a34f36651e13
Jared <jbeckersalax9@aol.com>
2021-12-07 18:09:15 -0500

    Done.

--- index.js
+++ index.js
@@ -24,13 +24,26 @@
 
 
 
-const names = ['Guadalupe', 'Ollie', 'Aki']
-const e = 'surprise'
+// const names = ['Guadalupe', 'Ollie', 'Aki']
+// const e = 'surprise'
+
 function writeCards(names, event){
+    let greeting = []
     for(let index = 0; index < names.length; index++){
-        let greeting = [...names];
-        return Array = [`Thank you, ${names[index]}, for the wonderful ${event} gift!`]
+        greeting.push(`Thank you, ${names[index]}, for the wonderful ${event} gift!`)
     }
+    return greeting
 };
 
-//[`Thank you, ${names[index]}, for the wonderful ${event} gift!`]
+
+
+
+function countDown(){
+    let integer = 10;
+    while(integer >= 0){
+        console.log(integer--);
+    };
+
+};
+
+
