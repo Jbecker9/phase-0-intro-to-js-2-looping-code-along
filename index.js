841bf3357c1d27c87069a6135967f0eba04cafc8
Jared <jbeckersalax9@aol.com>
2021-12-07 17:18:40 -0500

    Done.

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const names = ['Guadalupe', 'Ollie', 'Aki']
 const e = 'surprise'
 function writeCards(names, event){
     for(let index = 0; index < names.length; index++){
-        let writeCards = [...names]
-        return writeCards = [`Thank you, ${names[index]}, for the wonderful ${event} gift!`]
+        let greeting = [...names];
+        return Array = [`Thank you, ${names[index]}, for the wonderful ${event} gift!`]
     }
 };
 
